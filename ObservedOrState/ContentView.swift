39d55cec4e5f8e8b813f1564c1e7a60b9ebfddad
Vina Melody <vina_melody@yahoo.com>
2024-05-20 22:48:32 +0800

    Initial Commit

---
+++ ObservedOrState/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ObservedOrState
+//
+//  Created by Vina Melody on 20/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ObservedOrState/ObservedOrStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservedOrStateApp.swift
+//  ObservedOrState
+//
+//  Created by Vina Melody on 20/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ObservedOrStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
