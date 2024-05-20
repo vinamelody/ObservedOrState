e3978ed594b14fa1423543bd69123fe96770069d
Vina Melody <vina_melody@yahoo.com>
2024-05-20 23:20:07 +0800

    Setup ui

--- ObservedOrState/ContentView.swift
+++ ObservedOrState/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var rightCount: Int = 0
+    @State var leftCount: Int = 0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            VStack {
+                Text(verbatim: "\(leftCount)")
+                Button("Left", systemImage: "arrowshape.backward.circle", action: leftButtonTapped)
+            }
+            .padding()
+            .background(Color.yellow)
+
+            VStack {
+                Text(verbatim: "\(rightCount)")
+                Button(action: rightButtonTapped) {
+                    HStack {
+                        Text("Right")
+                        Image(systemName: "arrowshape.forward.circle")
+                    }
+                    .foregroundStyle(Color.white)
+                }
+
+            }
+            .padding()
+            .background(Color.green)
         }
         .padding()
+    }
+
+    func leftButtonTapped() {
+        leftCount += 1
+    }
+
+    func rightButtonTapped() {
+        rightCount += 1
     }
 }
 
